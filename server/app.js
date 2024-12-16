8113777729005213ba3292c5e2addb207869da35
Chetan Bohra <chetan.bohra@squareboat.com>
2024-12-16 12:51:35 +0530

    added deps + basecode

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const products = [
+  {
+    id: "PRODUCT_1",
+    name: 'Product 1',
+    price: 100
+  },
+  {
+    id: "PRODUCT_2",
+    name: 'Product 2',
+    price: 150
+  },
+  {
+    id: "PRODUCT_3",
+    name: 'Product 3',
+    price: 300
+  },
+];
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/products', (req, res) => {
+  res.json({ success: true, data: products });
+});
+
+const PORT = process.env.BE_PORT || 7071;
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on port ${PORT}`)
+})
